3ed620139eb9db57b5687eaaa086d5ea5441ca55
EstebanLizarraga <elizarraga_17@alu.uabcs.mx>
2020-11-03 13:22:02 -0700

    rutas controladores modelo y vista

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,10 @@ Route::get('/suma/{num1}/{num2}','WebController@suma');
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/books','BookController@index');
+
+Route::get('/categories', 'CategoryController@index');
+
+    
+
